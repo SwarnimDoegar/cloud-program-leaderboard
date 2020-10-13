144d9ca412790a4db183dfd05a6f5f02bf06b54e
Swarnim Doegar <swarnimdoegar@gmail.com>
2020-10-14 01:48:48 +0530

    removed one unwanted batch

--- core/tasks.py
+++ core/tasks.py
@@ -32,7 +32,6 @@ CHALLENGES_AVAILABLE = [
     'Set up and Configure a Cloud Environment in Google Cloud', 
     'Perform Foundational Infrastructure Tasks in Google Cloud', 
     'Getting Started: Create and Manage Cloud Resources',
-    'Google Cloud Essentials'
     ]
 
 
